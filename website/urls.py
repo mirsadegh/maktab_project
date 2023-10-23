0dd179d7ee00afab8438ff431f9aab34437bde4c
mirsadegh <mirsadeghporbehnam831@gmail.com>
2023-10-23 22:27:18 +0330

    created render html
    added template folder

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from . import views
 
 
 
 urlpatterns = [
+    path('', views.index_view),
+    path('about', views.about_view),
+    path('contact', views.contact_view),
     
 ]
 
