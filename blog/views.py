b377116d60071c1cf7d35746a233d4367b29a122
mirsadegh <mirsadeghporbehnam831@gmail.com>
2023-10-29 11:28:50 +0330

    add paginator in project

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,PageNotAnInteger, EmptyPage
 
 
 def blog_view(request,**kwargs):
@@ -9,7 +10,17 @@ def blog_view(request,**kwargs):
         
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username=kwargs['author_username'])
+        
+    posts = Paginator(posts,3)
+    try:    
+        page_number = request.GET.get('page') 
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
             
+                   
     context = { 'posts': posts }
     return render(request, 'blog/blog-home.html', context)
 
